d43f4f16aa453b0e25f859e4b3d08685c52f31f9
Andrew Wason <aw@motionbox.com>
2010-09-23 14:21:16 -0400

    Finish PNG saving - currently a blank PNG.

--- WebKitRender.mm
+++ WebKitRender.mm
@@ -13,6 +13,8 @@
 #include <base/basictypes.h>
 #include <base/message_loop.h>
 #include <skia/ext/platform_canvas.h>
+#include <skia/ext/bitmap_platform_device.h>
+#include <gfx/codec/png_codec.h>
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
@@ -32,11 +34,31 @@ int main (int argc, const char * argv[]) {
     webFrame->setCanHaveScrollbars(false);
     webFrame->loadRequest(WebKit::WebURLRequest(WebKit::WebURL(GURL("http://www.google.com/"))));
     webView->layout();
-    //XXX base::MessageLoop::current()->Run();
+    base::MessageLoop::current()->Run();
 
     skia::PlatformCanvas skiaCanvas(size.width, size.height, true);
     webView->paint(webkit_glue::ToWebCanvas(&skiaCanvas), WebKit::WebRect(0, 0, size.width, size.height));
 
+    {
+        // Get canvas bitmap
+        skia::BitmapPlatformDevice &skiaDevice = static_cast<skia::BitmapPlatformDevice&>(skiaCanvas.getTopPlatformDevice());
+        const SkBitmap &skiaBitmap = skiaDevice.accessBitmap(false);
+
+        // Encode pixel data to PNG.
+        std::vector<unsigned char> pngData;
+        SkAutoLockPixels bitmapLock(skiaBitmap);
+        gfx::PNGCodec::Encode(reinterpret_cast<const unsigned char*>(skiaBitmap.getPixels()),
+                              gfx::PNGCodec::FORMAT_BGRA, skiaBitmap.width(), skiaBitmap.height(),
+                              static_cast<int>(skiaBitmap.rowBytes()), false, &pngData);
+
+        // Write to disk. Cheat and be MacOS specific since this is just for testing.
+        NSData *data = [NSData dataWithBytesNoCopy:reinterpret_cast<void *>(&pngData[0])
+                                            length:pngData.size()
+                                      freeWhenDone:NO];
+        [data writeToFile:@"/tmp/render.png" atomically:NO];
+    }
+
+    // Shutdown
     webView->close();
     WebKit::shutdown();
 
