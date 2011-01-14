555a5cac89a3a4061e8a3549e3d8e10adab9e187
Andrew Wason <aw@motionbox.com>
2011-01-14 11:28:27 -0500

    Change structure of mlt service names.
    Set identifier and type on metadata automatically.

--- mlt/chromix_service.cc
+++ mlt/chromix_service.cc
@@ -39,7 +39,10 @@ const std::string chromix_get_metadata_dir() {
 //XXX http://mltframework.org/twiki/bin/view/MLT/MetadataRequirements
 static mlt_properties chromix_create_metadata(mlt_service_type service_type, const char* service_name, void*) {
     std::string metadata_path = chromix_get_metadata_dir() + service_name + YML_SUFFIX;
-	return mlt_properties_parse_yaml(metadata_path.c_str());
+	mlt_properties metadata = mlt_properties_parse_yaml(metadata_path.c_str());
+    mlt_properties_set(metadata, "identifier", service_name);
+    mlt_properties_set(metadata, "type", service_type_to_name(service_type));
+    return metadata;
 }
 
 static void* chromix_create_service(mlt_profile profile, mlt_service_type service_type, const char* service_name, const void*) {
@@ -87,7 +90,7 @@ static void* chromix_create_service(mlt_profile profile, mlt_service_type servic
 }
 
 void chromix_register_services(mlt_repository repository, mlt_service_type service_type) {
-    // Metadata is named "chromix.<service_name>.filter.yml" and the ID is "chromix.<service_name>.filter"
+    // Metadata is named e.g. "chromix.filter.<service_name>.yml" and the ID is "chromix.filter.<service_name>"
     // This is so we can have e.g. both a filter and transition with the same <service_name> name.
 
     const char* service_type_name = service_type_to_name(service_type);
@@ -96,9 +99,9 @@ void chromix_register_services(mlt_repository repository, mlt_service_type servi
 
     std::string chromix_metadata_dir(chromix_get_metadata_dir());
 
-    // "chromix.*.<service_type_name>.yml" e.g. "chromix.*.filter.yml"
-    std::string service_type_wildcard("chromix.*.");
-    service_type_wildcard.append(service_type_name).append(YML_SUFFIX);
+    // "chromix.<service_type_name>.*.yml" e.g. "chromix.filter.*.yml"
+    std::string service_type_wildcard("chromix.");
+    service_type_wildcard.append(service_type_name).append(".*" YML_SUFFIX);
 
     mlt_properties metadata_entries = mlt_properties_new();
     mlt_properties_dir_list(metadata_entries, chromix_metadata_dir.c_str(), service_type_wildcard.c_str(), 1);
@@ -106,7 +109,7 @@ void chromix_register_services(mlt_repository repository, mlt_service_type servi
     for (int i = 0; i < metadata_count; i++) {
         char* pathname = mlt_properties_get_value(metadata_entries, i);
 
-        // ID is "chromix.<service_name>.<service_type_name>" e.g. "chromix.bubbles.filter"
+        // ID is "chromix.<service_type_name>.<service_name>" e.g. "chromix.filter.bubbles"
         const char* id_start = &pathname[chromix_metadata_dir.length() + 1];
         int id_length = strlen(id_start) - (sizeof(YML_SUFFIX) - 1);
         std::string service_id(id_start, id_length);
