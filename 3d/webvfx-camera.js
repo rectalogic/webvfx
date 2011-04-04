53a550cca168b579c654ad6a6e8f2b43544b9181
Andrew Wason <rectalogic@webvfx.org>
2011-04-04 12:07:45 -0400

    Copyright notices.

--- 3d/webvfx-camera.js
+++ 3d/webvfx-camera.js
@@ -1,5 +1,6 @@
-// QML library
-.pragma library
+// Copyright (c) 2011 Hewlett-Packard Development Company, L.P. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 // range is an array of [beginX,endX] x coordinates that the endpoints
 // of this segment cover.
