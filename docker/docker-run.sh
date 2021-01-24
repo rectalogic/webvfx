56d7faec2c7bdde0688f217a0b691b3d03e3c117
Andrew Wason <rectalogic@rectalogic.com>
2021-01-24 16:46:13 -0500

    clean up demos

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ROOT=$(dirname "${BASH_SOURCE[0]}")
-MOUNT=${1:-"$(dirname "$(cd "${ROOT}"; pwd)/$(basename "$1")")"}
+MOUNT="$(dirname "$(cd "${ROOT}"; pwd)")"
 docker buildx build --tag webvfx "$ROOT"
 docker run --rm --init \
-    --mount="type=bind,src=${MOUNT},dst=/webvfx,consistency=cached" webvfx
+    --mount="type=bind,src=${MOUNT},dst=/webvfx,consistency=cached" webvfx "$@"
