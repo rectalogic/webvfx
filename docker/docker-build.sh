2dc804e983fa35db341f99201e290bdbf2dcf999
Andrew Wason <rectalogic@rectalogic.com>
2021-01-31 11:57:32 -0500

    caching

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ROOT=$(dirname "${BASH_SOURCE[0]}")
-docker buildx build --memory-swap -1 --tag webvfx "$ROOT"
+docker buildx build --memory-swap -1 --load --tag webvfx "$ROOT"
