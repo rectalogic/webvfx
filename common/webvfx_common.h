e644cfe945f8db2e9069288f733347dddf4f5f52
Andrew Wason <rectalogic@rectalogic.com>
2022-01-29 10:44:19 -0500

    Fix demo scripts. Use UINT32_MAX

--- common/webvfx_common.h
+++ common/webvfx_common.h
@@ -5,20 +5,19 @@
 #pragma once
 
 #include <arpa/inet.h>
+#include <stdint.h>
 #include <cmath>
 
 namespace WebVfxCommon {
     //! Number of bytes per pixel.
     static const int BytesPerPixel = 3;
 
-    static const uint32_t NanoSeconds = 1000000000;
-
     inline uint32_t toTimecode(uint32_t position, uint32_t duration) {
-        return htonl((uint32_t)round((position / (double)duration) * NanoSeconds));
+        return htonl((uint32_t)round((position / (double)duration) * UINT32_MAX));
     };
 
     inline double fromTimecode(uint32_t timecode) {
-        return ntohl(timecode) / (double)NanoSeconds;
+        return ntohl(timecode) / (double)UINT32_MAX;
     };
 
 }
