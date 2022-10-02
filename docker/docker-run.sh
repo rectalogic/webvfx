9fe8b2d4ed43a4813d580470190c81895ffb938c
Andrew Wason <rectalogic@rectalogic.com>
2022-10-02 15:55:22 -0400

    Clean up assets since they can differ between macos and linux

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 MOUNT="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)")"
+
+$MOUNT/../demo/mlt/tests/clean
+
 docker run ${DOCKER_OPTS} --rm --init \
     --mount="type=bind,src=${MOUNT},dst=/webvfx,consistency=cached" webvfx "$@"
