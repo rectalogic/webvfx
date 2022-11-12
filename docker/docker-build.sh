b83913a0a37e5529a27c15270809c3395495650b
Andrew Wason <rectalogic@rectalogic.com>
2022-11-12 12:05:47 -0500

    drop mlt plugin. simplify Dockerfile

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ROOT=$(dirname "${BASH_SOURCE[0]}")
-docker buildx build --memory-swap -1 --load --tag webvfx "$ROOT"
+docker buildx build --platform linux/amd64 --memory-swap -1 --load --tag webvfx "$ROOT"
