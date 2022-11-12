9375054884c105e723533317f333ebc24e5555bf
Andrew Wason <rectalogic@rectalogic.com>
2022-11-12 11:15:37 -0500

    update linux tests

--- webvfx/video_texture.cpp
+++ webvfx/video_texture.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Andrew Wason. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <QVideoFrame>
 #include <QVideoFrameFormat>
 #include <QVideoSink>
--- webvfx/video_texture.h
+++ webvfx/video_texture.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 Hewlett-Packard Development Company, L.P. All rights reserved.
+// Copyright (c) 2022 Andrew Wason. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
