a3033a367ad00f345beb0374021c70ac6e055958
Andrew Wason <rectalogic@rectalogic.com>
2023-04-29 14:00:36 -0400

    fixing linux tests

--- builders/linux/webvfx-run.sh
+++ builders/linux/webvfx-run.sh
@@ -8,9 +8,7 @@ cmake --install-prefix /usr/local/Qt/${QT_VER}/gcc_64 ../..
 cmake --build .
 cmake --install .
 
-cd /webvfx/demo/mlt/tests
 rm -rf /webvfx/build/linux/output/*
 mkdir -p /webvfx/build/linux/output
 export FREI0R_PATH=/usr/lib/frei0r-1/webvfx
-./runtests fixtures/linux /webvfx/build/linux/output
-exit $?
+exec "$@"
